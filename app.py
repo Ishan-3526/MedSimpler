e5ceb9a6d9d28d544b479baa084690879a4ea015
Ishan-3526 <ishandhamanikar@gmail.com>
2026-08-19 17:18:38 +0530

    Update import statements in app.py
    Refactored import statements for reports module.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from reports import ultrasound
 import streamlit as st
 import importlib
+
 import reports.blood_report as blood_report
-import reports.Medical_prescription as prescription
-from reports.X-ray_report import X_ray_report
-from reports.CT_SCAN import CT_SCAN
-from reports.MRI_reports import MRI_reports
-from reports.other import other
+import reports.medical_prescription as prescription
+import reports.ultrasound as ultrasound
+import reports.xray_report as xray_report
+import reports.ct_scan as ct_scan
+import reports.mri_report as mri_report
+import reports.other as other
 
 importlib.reload(blood_report)
 importlib.reload(prescription)
